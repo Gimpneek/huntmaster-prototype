c776eedaa6a7a4435e9305b33c274e2b4e614a6d
Colin Wren <colin@gimpneek.com>
2016-11-19 23:13:19 +0000

    - Adding Menu/Close menu name toggle

--- src/js/components/navigation/HamburgerMenu.jsx
+++ src/js/components/navigation/HamburgerMenu.jsx
@@ -3,16 +3,29 @@ import $ from "jquery"
 import sidebar from "../../../vendor/js/semantic-ui/sidebar"
 
 export default class HamburgerMenu extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            title: 'Menu'
+        }
+    }
+
     componentWillMount(){
         $.fn.sidebar.settings.context = '#huntmaster-container';
     }
     toggleMenu(){
+        if(this.state.title == 'Menu'){
+            this.setState({title: 'Close'})
+        }else{
+            this.setState({title: 'Menu'})
+        }
         $('.ui.sidebar').sidebar('toggle');
     }
 
     render(){
+        const {title} = this.state;
         return(
-            <div className="ui right item" onClick={this.toggleMenu.bind(this)}>Menu</div>
+            <div className="ui right item" onClick={this.toggleMenu.bind(this)}>{title}</div>
         )
     }
 }
